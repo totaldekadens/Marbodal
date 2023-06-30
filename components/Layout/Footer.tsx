02fa5e287c9327041fc51ff05880a0933fa179c9
totaldekadens <angelica.moberg@hotmail.com>
2023-06-30 13:55:46 +0200

    Footer Accordion Mobile display

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,12 +3,14 @@ import Button from "../Buttons/Button";
 import { P } from "../Fonts";
 import Container from "./Container";
 import { navlinks, upper } from "@/utils/data";
+import { Disclosure } from "@headlessui/react";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 
 const Footer = () => {
   return (
     <Container className="py-14 bg-custom-200">
       {/* upper */}
-      <div className="flex w-full flex-col gap-8 sm:gap-0 sm:flex-row justify-around pb-12 border border-t-0 border-l-0 border-r-0 border-b-decor-50/40">
+      <div className="flex w-full flex-col gap-8 sm:gap-0 sm:flex-row justify-around pb-12 sm:border sm:border-t-0 sm:border-l-0 sm:border-r-0 sm:border-b-decor-50/40">
         {upper.map((object) => (
           <div className="flex gap-3 justify-center items-center flex-col w-">
             <img src={object.icon} />
@@ -23,8 +25,11 @@ const Footer = () => {
           </div>
         ))}
       </div>
+
       {/* middle */}
-      <div className="flex flex-col lg:flex-row pt-24 lg:pt-12 pb-12 gap-8 border border-t-0 border-l-0 border-r-0 border-b-decor-50/40 mb-6">
+
+      {/* Desktop */}
+      <div className="hidden sm:flex flex-col lg:flex-row pt-24 lg:pt-12 pb-12 gap-8 border border-t-0 border-l-0 border-r-0 border-b-decor-50/40 mb-6">
         <div className="flex flex-col items-center mb-28 lg:mb-0 lg:items-start w-full xl:w-1/4 lg:w-1/3 gap-3">
           <h3 className="text-xl text-center sm:text-left text-custom-300 font-semibold leading-5 ">
             Boka ett möte med en köksdesigner
@@ -57,6 +62,50 @@ const Footer = () => {
             </div>
           ))}
         </div>
+      </div>
+      {/* Mobile - Accordion */}
+      <div className="block sm:hidden">
+        {navlinks.map((section, i) => (
+          <Disclosure
+            as="div"
+            key={i}
+            className="border-b border-custom-300/20 py-6"
+          >
+            {({ open }) => (
+              <>
+                <h3 className="-my-3 flow-root">
+                  <Disclosure.Button className="flex w-full items-center justify-between bg-custom-200 py-3 text-sm text-custom-300 hover:text-gray-500">
+                    <span className="font-medium text-custom-300">
+                      {section.header}
+                    </span>
+                    <span className="ml-6 flex items-center">
+                      {open ? (
+                        <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                      ) : (
+                        <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                      )}
+                    </span>
+                  </Disclosure.Button>
+                </h3>
+                <Disclosure.Panel className="pt-6">
+                  <div className="space-y-4">
+                    {section.links.map((link, optionIdx) => (
+                      <div key={optionIdx} className="flex items-center">
+                        <Link
+                          className="text-sm text-custom-300 mt-2"
+                          key={i}
+                          href={link.href}
+                        >
+                          {link.title}
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        ))}
       </div>
       {/* lower */}
       <div className="flex justify-between">
