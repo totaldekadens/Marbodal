b50402da47d660497745353b5d963700e4a1f9f2
totaldekadens <angelica.moberg@hotmail.com>
2023-06-30 11:34:32 +0200

    Hero slide show

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,24 +1,31 @@
+import clsx from "clsx";
 import Button from "./Buttons/Button";
+
+const list = [
+  { src: "/hero.png", alt: "Bild på kök", class: "slide--1" },
+  { src: "/hero2.png", alt: "Bild på kök", class: "slide--2" },
+  { src: "/hero3.png", alt: "Bild på kök", class: "slide--3" },
+];
 
 const Hero = () => {
   // Todo : Use next/image instead
   return (
-    <div className="relative w-full -mt-16  h-[700px] sm:h-[700px] max-h-screen md:h-[700px] z-0">
-      <img
-        className="object-cover h-full w-full"
-        src={"/hero.png"}
-        alt={"Bild på kök"}
-      />
-      <img
-        className="absolute inset-0 h-full w-full object-fill hidden md:flex "
-        src={"/overlayHero.png"}
-        alt={"overlay"}
-      />
-      <img
-        className="absolute inset-0 h-full w-full object-fill flex md:hidden "
-        src={"/overlayHeroMobile.png"}
-        alt={"overlay"}
-      />
+    <div className="slides">
+      {list.map((image, i) => (
+        <div key={i} className={clsx(`slide`, image.class)}>
+          <img className="slide__image" src={image.src} alt={image.alt} />
+          <img
+            className="absolute inset-0 h-full w-full object-fill hidden md:flex "
+            src={"/overlayHero.png"}
+            alt={"overlay"}
+          />
+          <img
+            className="absolute inset-0 h-full w-full object-fill flex md:hidden "
+            src={"/overlayHeroMobile.png"}
+            alt={"overlay"}
+          />
+        </div>
+      ))}
       <div className="absolute flex inset-0 text-custom-50 items-center justify-center font-serif gap-3 flex-col ">
         <div className="flex flex-col items-center gap-4 justify-center w-3/4 xl:w-1/2">
           <h1 className="text-[44px] sm:text-[55px] md:text-[66px] lg:text-[77px] text-center">
